d0948665c4385dfe8f2e3ea9f207492a5b2fc546
dddddgz <1978563501@qq.com>
2023-10-22 09:44:07 +0800

    添加一个软件

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 function show_inputer(element, text) {
     // 显示打字机特效
     element.innerText = "";
-    for (let subscript = 0; subscript < text.length; subscript += 1) {
+    for (let subscript = 0; subscript <= text.length; subscript += 1) {
         setTimeout(function () {
             element.innerText = text.slice(0, subscript);
-            if (subscript % 2 == text.length % 2) element.innerText += "|";
+            if (subscript % 2 != text.length % 2) element.innerText += "|";
         }, (subscript + 1) * 70);
     }
 }
