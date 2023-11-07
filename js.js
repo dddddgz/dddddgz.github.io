d285657752d0054c7f3fda18cd7c68819148a454
dddddgz <1978563501@qq.com>
2023-11-07 18:18:53 +0800

    update

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-function show_inputer(element, text) {
+function show_typewriter(element, text) {
     // 显示打字机特效
     element.innerText = "";
     for (let subscript = 0; subscript <= text.length; subscript += 1) {
@@ -6,14 +6,5 @@ function show_inputer(element, text) {
             element.innerText = text.slice(0, subscript);
             if (subscript % 2 != text.length % 2) element.innerText += "|";
         }, (subscript + 1) * 70);
-    }
-}
-
-function check_link() {
-    // 处理链接
-    let elements = document.getElementsByTagName("a");
-    for (let i = 0; i < elements.length; i += 1) {
-        let element = elements[i];
-        element.innerText += " (" + element.getAttribute("href").split("/")[2] + ")";
     }
 }
