25b9ccfb39ade396be921a2842f5162007eb0834
dddddgz <1978563501@qq.com>
2025-05-25 10:54:26 +0800

    7

--- main.js
+++ main.js
@@ -26,36 +26,48 @@ function addToNavbar(text, url) {
 // 添加导航栏标题
 navbar.appendChild(newElement('<a class="title" href="/">dddddgz 的个人网站</a>'));
 addToNavbar('主页', '/');
-addToNavbar('友情链接', '/friendlink.html');
 addToNavbar('关系测试（未完工）', '/relationship.html');
+addToNavbar('网站历史', '/history.html');
 addToNavbar('拼音工具', '/pinintool.html');
-addToNavbar('↗ View on GitHub', 'https://github.com/dddddgz/dddddgz.github.io');
+addToNavbar('友情链接', '/friendlink.html');
+addToNavbar('↗ Follow (GitHub)', 'https://github.com/dddddgz');
 addToNavbar('↗ FishC 账号', 'https://fishc.com.cn/space-uid-1518166.html');
 
 // 接下来的都是处理编写 HTML 时简化的部分
 // 加上备案
-$('#main').appendChild(newElement('<a href="https://icp.gov.moe/?keyword=20255504" target="_blank">萌ICP备20255504号</a>'));
-// 使鼠标悬浮在链接上时显示链接真实地址
+$('#icp').innerHTML = '备案：<a href="https://icp.gov.moe/?keyword=20255504" target="_blank">萌ICP备20255504号</a>';
+// 很多功能都简化到 <a> 里去了
 $all('a').forEach(element => {
-    if ((href = element.getAttribute('href')) && href.startsWith('https://')) {
+    if (uid = element.getAttribute('uid')) {
+        // 这个链接要指向一个 FishC 用户
+        element.setAttribute('href', `https://fishc.com.cn/space-uid-${uid}.html`);
+    } else if (tid = element.getAttribute('tid')) {
+        // 指向一个主题
+        if (pid = element.getAttribute('pid')) {
+            // 具体到主题中的一个帖子
+            element.setAttribute('href', `https://fishc.com.cn/forum.php?mod=redirect&goto=findpost&ptid=${tid}&pid=${pid}`);
+        } else {
+            element.setAttribute('href', `https://fishc.com.cn/thread-${tid}-1-1.html`);
+        }
+    }
+    if ((content = element.innerHTML) && !(href = element.getAttribute('href'))) {
+        // 链接地址和其内容相同，由 js 自动补充完整
+        element.setAttribute('href', (href = 'https://' + content));
+    }
+    if (href && href.startsWith('https://')) {
         // 链接通向站外，添加提示
         element.setAttribute('title', href);
     }
 });
-// 处理简化的链接（<fcpost>、<fcuser>）
-$all('fcpost').forEach(element => {
-    let url, tid = element.getAttribute('tid');
-    if (pid = element.getAttribute('pid')) {
-        // 指定了具体帖子，而不是仅仅一个主题
-        url = `https://fishc.com.cn/forum.php?mod=redirect&goto=findpost&ptid=${tid}&pid=${pid}`;
-    } else {
-        url = `https://fishc.com.cn/thread-${tid}-1-1.html`;
+// 脚注
+let notes = $all('#note li'), i = 0;
+$all('nt').forEach(element => {
+    if (nid = element.getAttribute('nid')) {
+        // 手动指定脚注序号
+        i = nid - 1;
     }
-    element.setAttribute('onclick', `window.location.href = '${url}';`);
-    element.setAttribute('title', `FishC 论坛的帖子（${url}）`);
-});
-$all('fcuser').forEach(element => {
-    let url = `https://fishc.com.cn/space-uid-${element.getAttribute('uid')}.html`;
-    element.setAttribute('onclick', `window.location.href = '${url}';`);
-    element.setAttribute('title', `FishC 论坛的用户（${url}）`);
+    // innerText 只显示元素显示的文本，不显示具体 HTML 代码
+    element.setAttribute('title', notes[i].innerText);
+    // 序号从 1 开始，刚好可以 ++i（++i 等效于 i = i + 1）
+    element.innerHTML = `<sup><a href="${href = notes[i].querySelector('a').getAttribute('href')}">[${++i}]</a></sup>`;
 });
